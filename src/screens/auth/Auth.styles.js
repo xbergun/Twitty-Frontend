e5b1d35e06f629149164f9244743c0a93fa10c74
Berkay <berkay.ergun16@gmail.com>
2023-04-02 16:18:13 +0300

    Logic react hook form validation

---
+++ src/screens/auth/Auth.styles.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+    input : {
+        width: "80%",
+        alignSelf: "center",
+        borderWidth: 1,
+        borderColor: "black",
+        height: 32,
+        paddingLeft : 10
+    },
+});
