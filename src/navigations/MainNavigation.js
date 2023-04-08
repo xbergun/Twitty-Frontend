839ecf3c8269316c59dae192474267f5660eb9cc
Berkay <berkay.ergun16@gmail.com>
2023-04-08 21:53:37 +0300

    status bar style added

--- src/navigations/MainNavigation.js
+++ src/navigations/MainNavigation.js
@@ -3,11 +3,18 @@ import BottomTabNavigation from "./BottomTabNavigation";
 import { useSelector } from "react-redux";
 import AuthNavigation from "./AuthNavigation";
 import DrawerNavigation from "./DrawerNavigation";
+import { StatusBar } from "react-native";
+import { useColorMode } from "native-base";
 
 export default MainNavigation = () => {
   const { userData } = useSelector((state) => state.auth);
+  const { colorMode} = useColorMode();
 
-  return <>{userData ? (
-    <DrawerNavigation />
-    ) : <AuthNavigation />}</>;
+  return (
+    <>
+      <StatusBar barStyle={colorMode === 'dark' ? 'light-content' : 'dark-content'} />
+      {userData ? <DrawerNavigation /> : <AuthNavigation />}
+    </>
+  );
+
 };
